8b2b239c8c5fa9b9e66b2382111d94dc518bf49d
Comet <comet@Comets-Mac.local>
2019-03-31 08:33:29 -0700

    Initial Commit

---
+++ TwitterVideoDownloader/Views/VideoListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwitterVideoDownloader
+//
+//  Created by Comet on 3/30/19.
+//  Copyright © 2019 SmoothAce. All rights reserved.
+//
+
+import UIKit
+
+class VideoListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
---
+++ TwitterVideoDownloader/Views/VideoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  VideoTableViewCell.swift
+//  TwitterVideoDownloader
+//
+//  Created by Comet on 3/30/19.
+//  Copyright © 2019 SmoothAce. All rights reserved.
+//
+
+import UIKit
+
+class VideoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
